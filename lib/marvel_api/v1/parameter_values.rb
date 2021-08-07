43fc1978da032451fb6e0d1c3ede8b9bd23c3c31
Heitor Carvalho <heitor@guava.com.br>
2021-08-07 20:10:03 -0300

    Move API parameter values to their own module

---
+++ lib/marvel_api/v1/parameter_values.rb
@@ -0,0 +1,31 @@
+module MarvelApi
+  module V1
+    module ParameterValues
+      THUMBNAIL_SIZES = {
+        small: 'portrait_small',
+        medium: 'portrait_xlarge',
+        large: 'portrait_uncanny',
+        original: 'detail'
+      }.freeze
+
+      SORT_PARAMETER = {
+        foc_date: 'focDate',
+        on_sale_date: 'onsaleDate',
+        title: 'title',
+        issue_number: 'issueNumber',
+        modified: 'modified',
+      }.freeze
+
+      SORT_DIRECTION = {
+        asc: '',
+        desc: '-'
+      }.freeze
+
+      FORMAT_TYPE = {
+        comic: 'comic',
+        collection: 'collection',
+        any: ''
+      }.freeze
+    end
+  end
+end
