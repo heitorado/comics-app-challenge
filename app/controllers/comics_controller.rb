a6046e9323246358f9b7107b9b3042f4417a6777
Heitor Carvalho <heitor@guava.com.br>
2021-08-08 20:48:54 -0300

    Add comics filtering by character name

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,12 +1,22 @@
 class ComicsController < ApplicationController
   def index
     comics_service = Marvel::ComicsService.new
-    @comics = comics_service.get_comics(page: comic_params[:page].to_i, limit: 100)
+    @comics = comics_service.get_comics(page: comic_params[:page].to_i)
+  end
+
+  def search_by_character
+    redirect_to root_path and return if comic_params[:character_name_query].blank?
+
+    comics_service = Marvel::ComicsService.new
+    characters = comics_service.get_characters(page: comic_params[:page].to_i, name_starts_with: comic_params[:character_name_query])
+    @comics = comics_service.get_comics(page: comic_params[:page].to_i, character_ids: characters.map(&:id))
+
+    render :index
   end
 
   private 
 
   def comic_params
-    params.permit(:page)
+    params.permit(:page, :character_name_query, :commit)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'comics#index'
+  get '/search_by_character', to: 'comics#search_by_character'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
