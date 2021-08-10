7ab845c3cb53d52c068aac959af538add4942883
Heitor Carvalho <heitor@guava.com.br>
2021-08-10 03:32:39 -0300

    Remove debug message and blank lines

--- app/javascript/favourite.js
+++ app/javascript/favourite.js
@@ -1,13 +1,8 @@
 $(document).on('turbolinks:load', function() {
-
-  console.log('hi');
-
   $('.favourite-button-section').on ("ajax:success", toggleFavouriteButton)
 
   function toggleFavouriteButton(event){
     var data = event.detail[0]
     $(this).parent().find('.favourite-button-section').html(data.favourite_button);
   }
-
 });
-
